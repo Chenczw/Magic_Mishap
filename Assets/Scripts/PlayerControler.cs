12f20dec5c3f4e15b203193470ea7521d13acf3b
czw <lchu976@gmail.com>
2020-12-03 22:41:30 -0500

    level 1 audio

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -37,7 +37,7 @@ public class PlayerControler : MonoBehaviour
         {
             rBody.AddForce(new Vector2(0.0f, jumpForce));
             isGrounded = false;
-            // GetComponent<AudioSource>().Play();
+            GetComponent<AudioSource>().Play();
         }
 
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
