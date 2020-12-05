bb1da93506d66cbc230cef402126a79d0bd43d40
czw <lchu976@gmail.com>
2020-12-04 21:30:18 -0500

    delete unused files

--- Assets/Scripts/frog.cs
+++ Assets/Scripts/frog.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class frog : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        if (other.CompareTag("Player"))
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
